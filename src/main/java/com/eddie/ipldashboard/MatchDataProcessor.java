d02084ca32f60710f4f7a7be138ee87fbea8b966
eddie-coding <adityabahuguna007@gmail.com>
2022-08-31 14:19:12 +0530

    Adding Spring Batch processor

---
+++ src/main/java/com/eddie/ipldashboard/MatchDataProcessor.java
@@ -0,0 +1,49 @@
+package com.eddie.ipldashboard;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.eddie.ipldashboard.model.Match;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+  private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+  @Override
+  public Match process(final MatchInput matchInput) throws Exception {
+
+    Match match = new Match();
+    match.setId(Long.parseLong(matchInput.getID()));
+    match.setCity(matchInput.getCity());
+    match.setDate(LocalDate.parse(matchInput.getDate()));
+    match.setPlayerOfMatch(matchInput.getPlayer_of_Match());
+    match.setVenue(matchInput.getVenue());
+
+    String firstInnningsTeam, secondInningsTeam;
+
+    if("bat".equals(matchInput.getTossDecision())){
+        firstInnningsTeam = matchInput.getTossWinner();
+        secondInningsTeam = matchInput.getTossWinner().equals(matchInput.getTeam1()) ? matchInput.getTeam2() : matchInput.getTeam1();
+    }
+    else{
+        secondInningsTeam = matchInput.getTossWinner();
+        firstInnningsTeam = matchInput.getTossWinner().equals(matchInput.getTeam1()) ? matchInput.getTeam2() : matchInput.getTeam1();
+    }
+
+    match.setTeam1(firstInnningsTeam);
+    match.setTeam2(secondInningsTeam);
+    match.setTossWinner(matchInput.getTossWinner());
+    match.setTossDecision(matchInput.getTossDecision());
+    match.setResult(matchInput.getWonBy());
+    match.setResultMargin(matchInput.getMargin());
+    match.setUmpire1(matchInput.getUmpire1());
+    match.setUmpire2(matchInput.getUmpire2());
+
+    return match;
+
+    }
+}
