78a73c68f4c4dbb4f0599c796add4289fafd3370
eddie-coding <adityabahuguna007@gmail.com>
2023-09-06 23:06:27 +0530

    Added TeamPage onto the frontend to fetch the team data from backend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage/>}/>
           <Route path="/teams/:teamName" element={<TeamPage/>}/>
         </Routes>
       </Router>
