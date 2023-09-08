c4ea4eaf595e4fa8b8a664dc5261107a7470980a
eddie-coding <adityabahuguna007@gmail.com>
2023-09-09 03:37:01 +0530

    Added home page for the application

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,15 +2,17 @@ import './App.scss';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
+      <Routes>
           <Route path="/teams/:teamName/matches/:year" element={<MatchPage/>}/>
           <Route path="/teams/:teamName" element={<TeamPage/>}/>
-        </Routes>
+          <Route path="/teams" element={<HomePage/>}/>
+        </Routes>    
       </Router>
     </div>
   );
